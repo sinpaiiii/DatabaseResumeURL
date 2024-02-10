dcf5c7b63d92ca972dd8f643388e66b987146b42
sinpaiiii <mohammednowfal7228@gmail.com>
2024-02-10 15:19:08 +0530

    version 2.0

--- FunResumeURL.js
+++ FunResumeURL.js
@@ -8,7 +8,23 @@ function LISTING() {
     let File = Files.next();
     result.push([File.getName().slice(0, -4), File.getUrl()]);
   }
-  SpreadsheetApp.getActiveSheet()
-    .getRange(1, 1, result.length, result[0].length)
-    .setValues(result);
+  //SpreadsheetApp.getActiveSheet().getRange(1,1,result.length,result[0].length).setValues(result)  //This is to create a new sheet with just the name and resume url
+  let curCell = SpreadsheetApp.getCurrentCell();
+  let curRow = curCell.getRow();
+  let curCol = curCell.getColumn();
+  //Logger.log(SpreadsheetApp.getActiveSheet().getRange(5, 1).getValue())
+  while (SpreadsheetApp.getActiveSheet().getRange(curRow, 2).getValue()) {
+    for (var i = 0; i < result.length; i++) {
+      if (
+        result[i][0] ==
+        SpreadsheetApp.getActiveSheet().getRange(curRow, 2).getValue()
+      ) {
+        SpreadsheetApp.getActiveSheet()
+          .getRange(curRow, curCol)
+          .setValue(result[i][1]);
+        break;
+      }
+    }
+    curRow += 1;
+  }
 }
